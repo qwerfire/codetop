044f92a1ae96ed8c8698c5ce47f361c8ede763a0
qwerfire <zxy152104410@163.com>
2022-10-22 01:07:55 +0800

    删除多余信息，只保留力扣需要的代码

--- CodeNo160.cpp
+++ CodeNo160.cpp
@@ -1,12 +1,3 @@
-#include <iostream>
-using namespace std;
-
- struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
